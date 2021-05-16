15b28051a2fd9cad5584243b2520236b97d15fa0
spsapa <ytyonti@gmail.com>
2021-05-16 08:53:15 +0000

    ㅈㄹ

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang='ko'>
+  <head>
+  	<meta charset="utf-8">
+	  <link rel="preconnect" href="https://fonts.gstatic.com">
+	  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+	  <meta name="viewport" content="width=750px">
+	  <title>커미션 사이트(?)</title>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+	  <link href="header.css" rel="stylesheet" type="text/css">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <script src="https://kit.fontawesome.com/a27819b7d3.js" crossorigin="anonymous"></script>
+</head>
+  <body>
+  <?php include("header.html");?>
+  <div class='conbutton'>
+  <h1>일단 여기에 뭔가를 쳐넣긴 할건데</h1>
+  <h1>그냥 트위터 크롤링이나해서 텍스트 쳐넣어버릴까</h1>
+  </div>
+  </body>
+</html>
